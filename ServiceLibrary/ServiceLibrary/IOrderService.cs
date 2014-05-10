af9d5512bfaa225cffb840c8a5cefb7c28138074
brownry <brown.ryanr@gmail.com>
2014-05-10 16:32:53 -0500

    RB: Add Service Library project for receiving requests from the client and forwarding them to the Order Matching Engine

---
+++ ServiceLibrary/ServiceLibrary/IOrderService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ClassLibrary;
+
+namespace ServiceLibrary
+{
+    [ServiceContract]
+    public interface IOrderService
+    {
+        [OperationContract]
+        Order SubmitOrder(Order order);
+
+        [OperationContract]
+        List<Order> GetOrders(Int64 traderId);
+
+        [OperationContract]
+        void CancelOrder(Int64 traderId, Int64 orderNumber);
+
+        /*
+        [OperationContract]
+        decimal GetAskPrice();
+
+        [OperationContract]
+        decimal GetBidPrice();
+        */
+    }
+
+}
---
+++ ServiceLibrary/ServiceLibrary/OrderService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using ClassLibrary;
+
+namespace ServiceLibrary
+{
+
+    public class OrderService : IOrderService
+    {
+        public Order SubmitOrder(Order order)
+        {
+            Message message = new Message(MessageType.SubmitOrder, order);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+            Console.WriteLine("Received Response - Message Type: {0}", response.messageType);
+            Console.WriteLine("Received Response - Order: {0}", (Order)response.payload);
+            System.Console.WriteLine("Order Submitted: {0}", order.ToString());
+            return (Order)response.payload;
+        }
+
+        public List<Order> GetOrders(Int64 traderId)
+        {
+            Message message = new Message(MessageType.RetrieveOrders, traderId);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+            Console.WriteLine("Received Response - Message Type: {0}", response.messageType);
+            Console.WriteLine("Received Response - Order: {0}", (List<Order>)response.payload);
+            List<Order> orders = (List<Order>)response.payload;
+            foreach (Order order in orders)
+            {
+                System.Console.WriteLine("Order Retrieved: {0}", order.ToString());
+            }
+            return orders;
+        }
+
+        public void CancelOrder(Int64 traderId, Int64 orderNumber)
+        {
+            Message message = new Message(MessageType.CancelOrder, orderNumber);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+            Console.WriteLine("Received Response - Message Type: {0}", response.messageType);
+            Console.WriteLine("Received Response - Order: {0}", (Int64)response.payload);
+        }
+
+        /*
+        public decimal GetAskPrice()
+        {
+            return OrderBook.getAskPrice();
+        }
+
+        public decimal GetBidPrice()
+        {
+            return OrderBook.getBidPrice();
+        }
+        */
+    }
+}
