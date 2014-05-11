27498ba48a1b2c776e66ccb5a7b728fbbb595200
brownry <brown.ryanr@gmail.com>
2014-05-11 13:51:24 -0500

    RB: Adding Trading Simulator project

---
+++ TradingSimulator/TradingSimulator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace Trader
+{
+    class PriceSim
+    {
+        public double SimulateAsset(double so, double mu, double sigma, double tau, double delta, Rand g)
+        {
+            double s = so;
+            double nsteps = tau;
+            for (int i = 0; i < (int)nsteps; i++)  //we could loop inside here for the price, I did it outside, but could easily remove
+            {
+                double r = g.Gauss(0.0, 1.0); //generate a gaussian random number mean 0 std. dev 1.0
+                s = s * (1.0 + mu * delta + sigma * r * Math.Sqrt(delta)); //notice we are adjusting annual mu and sigma for delta (i.e. time)
+            }
+            return s;
+        }
+
+    }
+
+    class Rand : Random
+    {
+        private double v1;
+        private double v2;
+        private double r;
+
+        public double Gauss(double mu, double sigma)
+        {
+            while (true)
+            {
+                v1 = NextDouble()*2.0-1.0;
+                v2 = NextDouble()*2.0-1.0;
+                r = (v1 * v1) + (v2 * v2);
+                if (r<1)
+                    break;
+            }
+            return mu + sigma * Math.Sqrt(-2.0 * Math.Log(r) / r) * v1;
+        }
+
+
+    }
+    class Pricing
+    {
+        static void Testing1()
+        {
+            Rand gaussiannumber = new Rand();
+            PriceSim testing = new PriceSim();
+            double updatedprice = 100.0;  //Starting price
+
+            for (int i = 0; i < 10; i++) //let the price walk for 10 days
+            {
+                updatedprice = testing.SimulateAsset(updatedprice, .10, .3, 1, 1.0/252.0, gaussiannumber);                
+            }
+            Console.WriteLine(updatedprice); //output price after ten days
+        }
+    }
+}
---
+++ TradingSimulator/TradingSimulator/Trader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using Trader;
+using ClassLibrary;
+
+namespace Simulation
+{
+    class Trader
+    {
+        public double startprice;
+        public double nextprice;
+        public double risklevel;
+        public double newsfreq;
+        public int loops;
+
+        private List<Order> buyOrders = new List<Order>();
+        private List<Order> sellOrders = new List<Order>();
+
+        private PriceSim walker;
+        private Rand g;
+
+        public Trader(double startprice, double risklevel, double newsfreq, int loops)
+        {
+            this.startprice = startprice;
+            this.walker = new PriceSim();
+            this.g = new Rand();
+            this.nextprice = walker.SimulateAsset(startprice, .02, .2, 1, 1.0 / 252.0, g);
+            this.risklevel = risklevel;
+            this.newsfreq = newsfreq;
+            this.loops = loops;
+        }
+
+        public void OrderGenerator()
+        {
+            for (int i = 0; i < loops; i++)
+            {
+                // Create new buy order
+                Order buyOrder = new Order();
+                buyOrder.buySell = BuySell.Buy;
+                buyOrder.productId = 1;
+                buyOrder.traderId = 0;
+                buyOrder.quantity = Convert.ToInt64(SimQtyLog(100, 10));
+                buyOrder.price = Convert.ToDecimal(SimPriceLog(nextprice * .90, 10.0));
+                buyOrders.Add(buyOrder);
+                SubmitOrder(buyOrder);
+
+                // Create new sell order
+                Order sellOrder = new Order();
+                sellOrder.buySell = BuySell.Sell;
+                sellOrder.productId = 1;
+                sellOrder.traderId = 1;
+                sellOrder.quantity = Convert.ToInt64(SimQtyLog(100, 10));
+                sellOrder.price = Convert.ToDecimal(SimPriceLog(nextprice * 1.10, 10.0));
+                sellOrders.Add(sellOrder);
+                SubmitOrder(sellOrder);
+            }
+        }
+
+        public double SimPriceLog(double mu, double sigma)
+        {
+            return Math.Round(g.Gauss(mu, sigma),2);
+        }
+
+        public double SimQtyLog(double mu, double sigma)
+        {
+            return Math.Round(g.Gauss(mu, sigma));
+        }
+
+        public void PrintOrders(BuySell buySell)
+        {
+            foreach(Order order in (buySell == BuySell.Buy ? buyOrders : sellOrders))
+            {
+                Console.WriteLine("Order: {0}", order.ToString());
+            }
+        }
+
+        public void SubmitOrder(Order order)
+        {
+            Message message = new Message(MessageType.SubmitOrder, order);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+        }
+
+        public void RetreiveOrders(Int64 traderId)
+        {
+            Message message = new Message(MessageType.RetrieveOrders, traderId);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+            foreach (Order order in (List<Order>)response.payload)
+            {
+                Console.WriteLine("Order: {0}", order.ToString());
+            }
+        }
+    }
+    class Simulated
+    {
+        static void Main()
+        {
+            Trader max = new Trader(100.0, 2.0, 50.0, 10);
+            max.OrderGenerator();
+            Console.WriteLine("These are the buy orders:");
+            max.PrintOrders(BuySell.Buy);
+            Console.WriteLine("These are the sell orders:");
+            max.PrintOrders(BuySell.Sell);
+            Console.WriteLine("These are the orders submitted by trader 0:");
+            max.RetreiveOrders(0);
+            Console.WriteLine("These are the orders submitted by trader 1:");
+            max.RetreiveOrders(1);
+            Console.Read();
+        }
+    }
+}
