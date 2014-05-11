081882deb2418d15427777e370f2f59f2a22dbd0
brownry <brown.ryanr@gmail.com>
2014-05-11 08:20:32 -0500

    RB: Add grid to display orders on form

--- ExchangeClient/ExchangeClient/SubmitOrderForm.Designer.cs
+++ ExchangeClient/ExchangeClient/SubmitOrderForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace OrderClient
+﻿namespace ExchangeClient
 {
     partial class SubmitOrderForm
     {
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.productLabel = new System.Windows.Forms.Label();
             this.quantityLabel = new System.Windows.Forms.Label();
             this.priceLabel = new System.Windows.Forms.Label();
@@ -35,12 +36,29 @@
             this.productValue = new System.Windows.Forms.TextBox();
             this.quantityValue = new System.Windows.Forms.TextBox();
             this.priceValue = new System.Windows.Forms.TextBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.orderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.orderBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.orderDataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewCheckBoxColumn3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dataGridViewCheckBoxColumn4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderDataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // productLabel
             // 
             this.productLabel.AutoSize = true;
-            this.productLabel.Location = new System.Drawing.Point(12, 9);
+            this.productLabel.Location = new System.Drawing.Point(11, 61);
             this.productLabel.Name = "productLabel";
             this.productLabel.Size = new System.Drawing.Size(44, 13);
             this.productLabel.TabIndex = 0;
@@ -49,7 +67,7 @@
             // quantityLabel
             // 
             this.quantityLabel.AutoSize = true;
-            this.quantityLabel.Location = new System.Drawing.Point(12, 50);
+            this.quantityLabel.Location = new System.Drawing.Point(11, 102);
             this.quantityLabel.Name = "quantityLabel";
             this.quantityLabel.Size = new System.Drawing.Size(46, 13);
             this.quantityLabel.TabIndex = 1;
@@ -58,7 +76,7 @@
             // priceLabel
             // 
             this.priceLabel.AutoSize = true;
-            this.priceLabel.Location = new System.Drawing.Point(12, 92);
+            this.priceLabel.Location = new System.Drawing.Point(11, 144);
             this.priceLabel.Name = "priceLabel";
             this.priceLabel.Size = new System.Drawing.Size(31, 13);
             this.priceLabel.TabIndex = 2;
@@ -66,7 +84,7 @@
             // 
             // submitButton
             // 
-            this.submitButton.Location = new System.Drawing.Point(15, 133);
+            this.submitButton.Location = new System.Drawing.Point(14, 185);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(75, 23);
             this.submitButton.TabIndex = 3;
@@ -76,30 +94,127 @@
             // 
             // productValue
             // 
-            this.productValue.Location = new System.Drawing.Point(63, 2);
+            this.productValue.Location = new System.Drawing.Point(62, 54);
             this.productValue.Name = "productValue";
             this.productValue.Size = new System.Drawing.Size(100, 20);
             this.productValue.TabIndex = 4;
             // 
             // quantityValue
             // 
-            this.quantityValue.Location = new System.Drawing.Point(63, 43);
+            this.quantityValue.Location = new System.Drawing.Point(62, 95);
             this.quantityValue.Name = "quantityValue";
             this.quantityValue.Size = new System.Drawing.Size(100, 20);
             this.quantityValue.TabIndex = 5;
             // 
             // priceValue
             // 
-            this.priceValue.Location = new System.Drawing.Point(63, 84);
+            this.priceValue.Location = new System.Drawing.Point(62, 136);
             this.priceValue.Name = "priceValue";
             this.priceValue.Size = new System.Drawing.Size(100, 20);
             this.priceValue.TabIndex = 6;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(14, 472);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 23);
+            this.refreshButton.TabIndex = 8;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // orderBindingSource1
+            // 
+            this.orderBindingSource1.DataSource = typeof(ClassLibrary.Order);
+            // 
+            // orderDataGridView1
+            // 
+            this.orderDataGridView1.AutoGenerateColumns = false;
+            this.orderDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.orderDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn9,
+            this.dataGridViewTextBoxColumn10,
+            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewTextBoxColumn12,
+            this.dataGridViewTextBoxColumn13,
+            this.dataGridViewTextBoxColumn14,
+            this.dataGridViewTextBoxColumn15,
+            this.dataGridViewTextBoxColumn16,
+            this.dataGridViewCheckBoxColumn3,
+            this.dataGridViewCheckBoxColumn4});
+            this.orderDataGridView1.DataSource = this.orderBindingSource1;
+            this.orderDataGridView1.Location = new System.Drawing.Point(14, 230);
+            this.orderDataGridView1.Name = "orderDataGridView1";
+            this.orderDataGridView1.Size = new System.Drawing.Size(717, 220);
+            this.orderDataGridView1.TabIndex = 10;
+            // 
+            // dataGridViewTextBoxColumn9
+            // 
+            this.dataGridViewTextBoxColumn9.DataPropertyName = "orderNumber";
+            this.dataGridViewTextBoxColumn9.HeaderText = "orderNumber";
+            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
+            // 
+            // dataGridViewTextBoxColumn10
+            // 
+            this.dataGridViewTextBoxColumn10.DataPropertyName = "orderType";
+            this.dataGridViewTextBoxColumn10.HeaderText = "orderType";
+            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
+            // 
+            // dataGridViewTextBoxColumn11
+            // 
+            this.dataGridViewTextBoxColumn11.DataPropertyName = "buySell";
+            this.dataGridViewTextBoxColumn11.HeaderText = "buySell";
+            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
+            // 
+            // dataGridViewTextBoxColumn12
+            // 
+            this.dataGridViewTextBoxColumn12.DataPropertyName = "productId";
+            this.dataGridViewTextBoxColumn12.HeaderText = "productId";
+            this.dataGridViewTextBoxColumn12.Name = "dataGridViewTextBoxColumn12";
+            // 
+            // dataGridViewTextBoxColumn13
+            // 
+            this.dataGridViewTextBoxColumn13.DataPropertyName = "quantity";
+            this.dataGridViewTextBoxColumn13.HeaderText = "quantity";
+            this.dataGridViewTextBoxColumn13.Name = "dataGridViewTextBoxColumn13";
+            // 
+            // dataGridViewTextBoxColumn14
+            // 
+            this.dataGridViewTextBoxColumn14.DataPropertyName = "price";
+            this.dataGridViewTextBoxColumn14.HeaderText = "price";
+            this.dataGridViewTextBoxColumn14.Name = "dataGridViewTextBoxColumn14";
+            // 
+            // dataGridViewTextBoxColumn15
+            // 
+            this.dataGridViewTextBoxColumn15.DataPropertyName = "submitTime";
+            this.dataGridViewTextBoxColumn15.HeaderText = "submitTime";
+            this.dataGridViewTextBoxColumn15.Name = "dataGridViewTextBoxColumn15";
+            // 
+            // dataGridViewTextBoxColumn16
+            // 
+            this.dataGridViewTextBoxColumn16.DataPropertyName = "traderId";
+            this.dataGridViewTextBoxColumn16.HeaderText = "traderId";
+            this.dataGridViewTextBoxColumn16.Name = "dataGridViewTextBoxColumn16";
+            // 
+            // dataGridViewCheckBoxColumn3
+            // 
+            this.dataGridViewCheckBoxColumn3.DataPropertyName = "cancelled";
+            this.dataGridViewCheckBoxColumn3.HeaderText = "cancelled";
+            this.dataGridViewCheckBoxColumn3.Name = "dataGridViewCheckBoxColumn3";
+            // 
+            // dataGridViewCheckBoxColumn4
+            // 
+            this.dataGridViewCheckBoxColumn4.DataPropertyName = "matched";
+            this.dataGridViewCheckBoxColumn4.HeaderText = "matched";
+            this.dataGridViewCheckBoxColumn4.Name = "dataGridViewCheckBoxColumn4";
             // 
             // SubmitOrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(783, 722);
+            this.Controls.Add(this.orderDataGridView1);
+            this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.priceValue);
             this.Controls.Add(this.quantityValue);
             this.Controls.Add(this.productValue);
@@ -109,6 +224,10 @@
             this.Controls.Add(this.productLabel);
             this.Name = "SubmitOrderForm";
             this.Text = "Submit Order";
+            this.Load += new System.EventHandler(this.SubmitOrderForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orderDataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,6 +242,30 @@
         private System.Windows.Forms.TextBox productValue;
         private System.Windows.Forms.TextBox quantityValue;
         private System.Windows.Forms.TextBox priceValue;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.BindingSource orderBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn2;
+        private System.Windows.Forms.BindingSource orderBindingSource1;
+        private System.Windows.Forms.DataGridView orderDataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn16;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn dataGridViewCheckBoxColumn4;
     }
 }
 
--- ExchangeClient/ExchangeClient/SubmitOrderForm.cs
+++ ExchangeClient/ExchangeClient/SubmitOrderForm.cs
@@ -9,13 +9,28 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClassLibrary;
 
-namespace OrderClient
+namespace ExchangeClient
 {
     public partial class SubmitOrderForm : Form
     {
+
         public SubmitOrderForm()
         {
             InitializeComponent();
+        }
+
+        private void SubmitOrderForm_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Loading Form");
+            OrderService.IOrderService service = new OrderService.OrderServiceClient();
+            List<Order> orders = service.GetOrders(0);
+
+            foreach (Order order in orders)
+            {
+                Console.WriteLine("Received Order: {0}", order.ToString());
+            }
+
+            orderDataGridView1.DataSource = orders;
         }
 
         private void submitButton_Click(object sender, EventArgs e)
@@ -32,6 +47,26 @@ namespace OrderClient
             System.Console.WriteLine("Order Submission Successful: Order Number = " + submittedOrder.orderNumber);
             orderService.CancelOrder(1234, submittedOrder.orderNumber);
             System.Console.WriteLine("Order Cancellation Successful: Order Number = " + submittedOrder.orderNumber);
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Refresh Button Clicked");
+
+            //ServiceLibrary.OrderService orderService = new ServiceLibrary.OrderService();
+            //List<Order> orders = orderService.GetOrders(0);
+
+            //OrderServiceReference.IOrderService service = new OrderServiceReference.OrderServiceClient();
+            //Order[] myOrders = service.GetOrders(0);
+
+            OrderService.IOrderService service = new OrderService.OrderServiceClient();
+            List<Order> orders = service.GetOrders(0);
+
+            foreach (Order order in orders)
+            {
+                Console.WriteLine("Received Order: {0}", order.ToString());
+            }
+            orderDataGridView1.DataSource = orders;
         }
 
     }
