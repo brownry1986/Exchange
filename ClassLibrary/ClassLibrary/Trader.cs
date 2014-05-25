a6a6009f227884afc1de8e61b88a01f09987e16e
brownry <brown.ryanr@gmail.com>
2014-05-25 17:50:58 -0500

    RB: Add Trader drop down and fake trader list

---
+++ ClassLibrary/ClassLibrary/Trader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Trader
+    {
+        public Trader(Int64 traderId, String name) 
+        {
+            this.traderId = traderId;
+            this.name = name;
+        }
+
+        public Int64 traderId { get; set; }
+
+        public String name { get; set; }
+
+    }
+}
---
+++ ClassLibrary/ClassLibrary/TraderList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class TraderList
+    {
+
+        public static List<Trader> traders = new List<Trader>();
+
+        static TraderList()
+        {
+            traders.Add(new Trader(0, "Max Gillman"));
+            traders.Add(new Trader(1, "Chris Freeman"));
+            traders.Add(new Trader(2, "Byron Housten"));
+            traders.Add(new Trader(3, "Michael Liu"));
+            traders.Add(new Trader(4, "Ryan Brown"));
+        }
+    }
+
+}
