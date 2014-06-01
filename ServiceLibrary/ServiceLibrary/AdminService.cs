a525f86584f40587918a45decad12811a168a8c8
brownry <brown.ryanr@gmail.com>
2014-06-01 12:39:54 -0500

    RB: Add admin service to expose service methods.

---
+++ ServiceLibrary/ServiceLibrary/AdminService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using ClassLibrary;
+
+namespace ServiceLibrary
+{
+
+    public class AdminService : IAdminService
+    {
+        public List<Order> GetOrders(Int64 productId)
+        {
+            Message message = new Message(MessageType.AdminRetrieveOrders, productId);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+            Console.WriteLine("Received Response - Message Type: {0}", response.messageType);
+            Console.WriteLine("Received Response - Order: {0}", (List<Order>)response.payload);
+            List<Order> orders = (List<Order>)response.payload;
+            foreach (Order order in orders)
+            {
+                System.Console.WriteLine("Order Retrieved: {0}", order.ToString());
+            }
+            return orders;
+        }
+
+        public List<Trade> GetTrades(Int64 productId)
+        {
+            Message message = new Message(MessageType.AdminRetrieveTrades, productId);
+            Socket socket = Messenger.SendMessage(message);
+            Message response = Messenger.ReceiveMessage(socket);
+            Console.WriteLine("Received Response - Message Type: {0}", response.messageType);
+            Console.WriteLine("Received Response - Order: {0}", (List<Trade>)response.payload);
+            List<Trade> trades = (List<Trade>)response.payload;
+            foreach (Trade trade in trades)
+            {
+                System.Console.WriteLine("Trade Retrieved: {0}", trade.ToString());
+            }
+            return trades;
+        }
+    }
+}
---
+++ ServiceLibrary/ServiceLibrary/IAdminService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ClassLibrary;
+
+namespace ServiceLibrary
+{
+    [ServiceContract]
+    public interface IAdminService
+    {
+        [OperationContract]
+        List<Order> GetOrders(Int64 productId);
+
+        [OperationContract]
+        List<Trade> GetTrades(Int64 productId);
+    }
+}
