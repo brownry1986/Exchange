780d6c8c721526564f18fffc581ab4e85d86bc91
brownry <brown.ryanr@gmail.com>
2014-06-01 12:40:24 -0500

    RB: Add Bid/Ask object for storing/communicating the bid/ask prices.

---
+++ ClassLibrary/ClassLibrary/BidAsk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace ClassLibrary
+{
+    [Serializable]
+    [DataContract]
+    public class BidAsk
+    {
+        public BidAsk(String bidPrice, String askPrice)
+        {
+            this.bidPrice = bidPrice;
+            this.askPrice = askPrice;
+        }
+
+        [DataMember]
+        public virtual String bidPrice { get; set; }
+
+        [DataMember]
+        public virtual String askPrice { get; set; }
+    }
+}
