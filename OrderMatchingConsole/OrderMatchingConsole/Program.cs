9134bc5b97ab7caa208d3d335f12a78403126cc6
brownry <brown.ryanr@gmail.com>
2014-06-07 12:35:47 -0500

    RB: Create Order Matching Console application for testing the Order Matching Library as a console application

---
+++ OrderMatchingConsole/OrderMatchingConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderMatchingLibrary;
+
+namespace OrderMatchingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderMatchingEngine orderMatchingEngine = new OrderMatchingEngine();
+            orderMatchingEngine.Start();
+        }
+    }
+}
